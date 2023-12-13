ce482338985001f99e3e4b793bff04751b508336
soorokim <soorokim@ent-bc.com>
2023-12-13 15:07:45 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button onClick={onClick}>test sentry</button>
+      <button onClick={onClick}>test sentry!</button>
     </main>
   );
 }
